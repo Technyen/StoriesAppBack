4c1b3afa0f235ff74272fa263ebcd0c55e5fdcbd
Peng <penghe201211@gmail.com>
2023-08-07 18:17:08 -0300

    deployment success

--- Api/Program.cs
+++ Api/Program.cs
@@ -57,11 +57,11 @@ namespace Api
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
