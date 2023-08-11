6ff1c4609d755cbc68bf353cabe657a0b9116843
Peng <penghe201211@gmail.com>
2023-08-11 13:35:45 -0300

    testing new branch

--- Api/Entities/User.cs
+++ Api/Entities/User.cs
@@ -5,6 +5,7 @@ namespace Api.Entities
     public class User
     {
         public string Type = nameof(User);
+        public int age { get; set; }    
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
