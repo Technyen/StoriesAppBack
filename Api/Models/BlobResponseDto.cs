67ce7ceb05598f7edc1fc3f42ae3877153df1340
Peng <penghe201211@gmail.com>
2023-09-05 16:56:33 -0300

    blob storage

---
+++ Api/Models/BlobResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Api.Models
+{
+    public class BlobResponseDto
+    {
+
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public BlobDto Blob { get; set; }
+
+        public BlobResponseDto()
+        {
+            Blob = new BlobDto();
+        }
+
+    }
+}
---
+++ Api/Services/IAzureStorage.cs
@@ -0,0 +1,12 @@
+﻿using Api.Models;
+namespace Api.Services
+{
+    public interface IAzureStorage
+    {
+        Task<BlobResponseDto> UploadAsync(IFormFile file);
+        Task<BlobDto> DownloadAsync(string blobFilename);
+        Task<BlobResponseDto> DeleteAsync(string blobFilename);
+        Task<List<BlobDto>> ListAsync();
+
+    }
+}
